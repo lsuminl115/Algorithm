f5bc7b529f40d6f8945cece45b3136fc248e6a4d
Sumin-Lee <97670920+lsuminl115@users.noreply.github.com>
2023-09-13 12:17:15 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("%s",a<2?"Before":a>2?"After":b<18?"Before":b>18?"After":"Special",scanf("%d %d",&a,&b));
+    return 0;
+}
